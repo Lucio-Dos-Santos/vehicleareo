73a43ce65d569c2a61307e3a28fa29a6f2de609d
Sam <unal.evan@gmail.com>
2021-06-13 22:38:48 +0200

    Update client.lua
    Ceci est un test

--- client.lua
+++ client.lua
@@ -14,10 +14,10 @@ local point = { ['heading'] = 153.63, ['x'] = -979.06, ['y'] = -2689.05, ['z'] =
 local blip = AddBlipForCoord(point.x, point.y, point.z)
 local vehSpawned = false
 local vehHash = -431692672
-SetBlipSprite (blip, 225)
+SetBlipSprite(blip, 225)
 SetBlipDisplay(blip, 4)
-SetBlipScale  (blip, 0.9)
-SetBlipColour (blip, 3)
+SetBlipScale(blip, 0.9)
+SetBlipColour(blip, 3)
 SetBlipAsShortRange(blip, true)
 BeginTextCommandSetBlipName("STRING")
 AddTextComponentString('Emprunt véhicule')
@@ -45,7 +45,6 @@ Citizen.CreateThread(function()
 					local vehicleareo = CreateVehicle(vehHash, point.x, point.y, point.z, point.heading, true, 1)
 					SetPedIntoVehicle(PlayerPedId(), vehicleareo, -1)
 					vehSpawned = true
-
 				end
 			end
 		end
